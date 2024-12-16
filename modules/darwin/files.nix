2428ccf3b5e6c1f81acfda1bb16e7bf8f4f8c3fb
Camden Cheek <camden@ccheek.com>
2024-12-15 19:57:33 -0700

    cleanups for personal computer

--- modules/darwin/files.nix
+++ modules/darwin/files.nix
@@ -1,10 +1,6 @@
 _:
 
 {
-  ".config/aerospace/aerospace.toml" = {
-    text = builtins.readFile ./config/aerospace.toml;
-  };
-
   # TODO: this is sourcegraph specific
   ".bin/bazel" = {
     text = ''
--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -8,13 +8,5 @@
       allowInsecure = false;
       allowUnsupportedSystem = true;
     };
-
-    overlays =
-      # Apply each overlay found in the /overlays directory
-      let path = ../../overlays; in with builtins;
-      map (n: import (path + ("/" + n)))
-          (filter (n: match ".*\\.nix" n != null ||
-                      pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)));
   };
 }
