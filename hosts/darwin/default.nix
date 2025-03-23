c6b493ac9f69ccd12d27f1a1f77c5acda5379ee0
Camden Cheek <camden@ccheek.com>
2025-03-23 15:48:07 -0600

    remove unused emacs daemon

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -55,18 +55,6 @@
   # Enable Sourcegraph-specific configuration if necessary
   my.sourcegraph.enable = locals.tags.work;
 
-  launchd.user.agents.emacs.path = [ config.environment.systemPath ];
-  launchd.user.agents.emacs.serviceConfig = {
-    KeepAlive = true;
-    ProgramArguments = [
-      "/bin/sh"
-      "-c"
-      "/bin/wait4path ${pkgs.emacs}/bin/emacs && exec ${pkgs.emacs}/bin/emacs --fg-daemon"
-    ];
-    StandardErrorPath = "/tmp/emacs.err.log";
-    StandardOutPath = "/tmp/emacs.out.log";
-  };
-
   system = {
     stateVersion = 4;
 
