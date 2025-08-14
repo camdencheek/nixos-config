d93feae3e7b35aed12ff3b68bdf1dba993a88ab6
Camden Cheek <camden@ccheek.com>
2025-08-14 14:16:01 -0600

    remove nvim config

--- modules/darwin/config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Keep your gx mapping for opening URLs
-vim.keymap.set("n", "gx", [[:silent execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]], { silent = true })
