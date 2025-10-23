9b7300eaef6d1a3a575b0426f533a9dd153e47f0
Camden Cheek <camden@ccheek.com>
2025-10-22 21:02:12 -0600

    flake update

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -2,98 +2,76 @@
 
 with pkgs;
 [
-  # General packages for development and system management
+# General packages for development and system management
 	p7zip
-  # mullvad-vpn
-  aspell
-  aspellDicts.en
-  atuin
-  bash-completion
-  bat
-  btop
-  coreutils
-  pkg-config
-  cmake
-  direnv
-  docker
-  localsend
-  docker-compose
-  duckdb
-  dtrx
-  eza
-  fre
-  fzf
-  git
-  git-open
-  github-cli
-  graphviz
-  jjui
-  jujutsu
-  killall
-  luarocks
-  luajit
-  mise
-  neovim
-  ngrok
-  nmap
-  openssh
-  parallel
-  podman
-  rbw
-  sqlite
-  wget
-  yq
-  zip
-  zstd
+	aspell
+	aspellDicts.en
+	atuin
+	bash-completion
+	bat
+	btop
+	coreutils
+	pkg-config
+	cmake
+	direnv
+	docker
+	localsend
+	docker-compose
+	duckdb
+	dtrx
+	eza
+	fre
+	fzf
+	git
+	git-open
+	github-cli
+	graphviz
+	jjui
+	jujutsu
+	killall
+	luarocks
+	luajit
+	mise
+	neovim
+	openssh
+	parallel
+	podman
+	sqlite
+	wget
+	yq
+	zip
+	zstd
 
-  # Language tools
-  nixd
-  nil # another nix language server
-  nixfmt-rfc-style
-  rustup
-  nodejs_24
-  pnpm
-  bun
-  vlc-bin
-  telegram-desktop
-  vscode
+# Language tools
+	vlc-bin
+	telegram-desktop
+	vscode
 
+# Encryption and security tools
+	gnupg
+	libfido2
+	nss.tools # includes certutil
 
-  # Encryption and security tools
-  age
-  age-plugin-yubikey
-  gnupg
-  libfido2
-  nss.tools # includes certutil
+# Media-related packages
+	dejavu_fonts
+	ffmpeg
+	fd
+	font-awesome
+	hack-font
+	noto-fonts
+	noto-fonts-emoji
+	intel-one-mono
+	meslo-lgs-nf
 
-  # Media-related packages
-  dejavu_fonts
-  ffmpeg
-  fd
-  font-awesome
-  hack-font
-  noto-fonts
-  noto-fonts-emoji
-  intel-one-mono
-  meslo-lgs-nf
-  pinentry-tty
-
-
-  # Text and terminal utilities
-  htop
-  hunspell
-  iftop
-  jetbrains-mono
-  jq
-  ripgrep
-  tree
-  unrar
-  unzip
-  zsh-powerlevel10k
-  tree-sitter
-
-  # Python packages
-  python312
-  virtualenv
-  uv
-]
+# Text and terminal utilities
+	htop
+	iftop
+	jetbrains-mono
+	jq
+	ripgrep
+	tree
+	unrar
+	unzip
+	zsh-powerlevel10k
+	tree-sitter
+	]
